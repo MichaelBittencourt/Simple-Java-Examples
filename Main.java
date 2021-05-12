9d85eceafebbbe59a90e303e599ddcf9c2f91e08
Michael Bittencourt <mchl.bittencourt@gmail.com>
2021-05-12 09:29:01 -0400

    feat: Implement a sum of a matrix

--- Main.java
+++ Main.java
@@ -1,19 +1,29 @@
 public class Main {
 
+    public static void printMatrix(Integer[][] matrix) {
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
 
-        int a = 1;
-        int b = 100212;
-        short d = 32767;
-        short c = (short) a;
-        short e = (short) d;
+        Integer[][] matrix = new Integer[][] {
+            {2, 4, 6, 8},
+            {1, 2, 3, 4},
+            {2, 4, 8, 16},
+            {3, 6, 9, 12}
+        };
 
-        System.out.println(c);
-        System.out.println(e);
+        System.out.println("My matrix:");
+        printMatrix(matrix);
 
-        Thread pool[] = new MyThread[5];
-        for (int i = 0; i < 5; i++) {
-            pool[i] = new MyThread(i);
+        Thread pool[] = new MyThread[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            pool[i] = new MyThread(i, matrix);
             pool[i].start();
         }
 
@@ -24,13 +34,27 @@ public class Main {
 class MyThread extends Thread {
 
     private Integer id;
+    private Integer[][] matrix;
 
-    public MyThread(Integer id) {
+    public MyThread(Integer id, Integer[][] matrix) {
         this.id = id;
+        this.matrix = matrix;
+    }
+
+    public void setMatrix(Integer[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    private Integer sumMatrixLine() {
+        Integer sum = 0;
+        for(int i = 0; i < matrix[id].length; i++) {
+            sum += matrix[id][i];
+        }
+        return sum;
     }
 
     public void run() {
-        System.out.println("My thread name is: " + id);
+        System.out.println("My thread name is: " + id + " sum: " + sumMatrixLine());
     }
 
 }
