4dd38cc4951b40033cac756bbca39e56cffb62a8
Michael Bittencourt <mabm@cesar.org.br>
2021-05-19 08:56:26 -0400

    first concurrent racer without barrier

--- Main.java
+++ Main.java
@@ -14,31 +14,52 @@ public class Main {
             pool[i] = new Lebre(i);
             pool[i].start();
         }
-
+        while (Lebre.QTD_FINISH < amountOfThreads);
+        System.out.println("Todos terminaram");
     }
 
 }
 
 class Lebre extends Thread {
 
-    private Integer id;
-    private Integer jump;
     private static Random randJump = new Random();
+    private static Integer first;
+    public static Integer QTD_FINISH = 0;
+    public static final Integer RACER_LENGTH = 100;
+    public static Integer MAX_JUMP = 20;
+
+    private Integer id;
+    private Integer position;
+
+    public void updatePosition() {
+        Integer jump = randJump.nextInt() % MAX_JUMP + 1;
+        jump = jump < 0 ? -jump : jump;
+        this.position += jump;
+    }
 
     public Lebre(Integer id) {
         this.id = id;
-        jump = randJump.nextInt() % 20;
-        jump = jump < 0 ? -jump : jump;
+        this.position = 0;
+    }
+
+    private static void updateFirst(Integer id) {
+        if (first == null) {
+            first = id;
+        }
+    }
+
+    private static void incrementAmount() {
+        QTD_FINISH++;
     }
 
     public void run() {
-        try {
-            Thread.sleep(jump * 100);
-        } catch(Exception e) { }
-
-        System.out.println("Lebre: " + id + " pulo: " + jump);
+        do {
+            this.updatePosition();
+            System.out.println("Lebre[" + id + "] = " + this.position);
+        } while(this.position < RACER_LENGTH);
+        Lebre.updateFirst(id);
+        System.out.println("Lebre[" + id + "] Terminou");
     }
-
 }
 
 
